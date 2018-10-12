80b163f964cdc6f6ca6fb96938d3f6d3e21bf455
Chikku2000 <chirag.raja.cr.786@gmail.com>
2018-10-12 10:29:38 +0530

    “Succesful”

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const port=process.env.PORT || 1900;
+const querystring = require('query-string');
+
+const sql = mysql.createConnection({
+  host : '127.0.0.1',
+  user : 'chirag',
+  password : 'chirag@248895',
+  database : 'post',
+  port : '3307'
+});
+// sql.query('SELECT COUNT(*) AS number FROM employee' , (err , reslt) =>{
+//   if(err) throw err;
+//   const n = reslt[0].number;
+//   console.log(n);
+// })
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine' , 'ejs');
+app.set('views' , './views');
+app.get('/home' , (req , res) => {
+  res.sendFile(path.join(__dirname , 'home.html'));
+});
+app.get('/register', (req , res )=>{
+  res.sendFile(path.join(__dirname ,'register.html'));
+});
+app.get('/search' ,(req , res)=>{
+  res.sendFile(path.join(__dirname , 'search.html'));
+});
+app.get('/remove' , (req , res)=>{
+  res.sendFile(path.join(__dirname , 'delete.html'))
+})
+app.post('/register' , (req ,res) =>{
+  console.log(req.body)
+  res.end();
+  sql.query('INSERT INTO employee(Name , Email , Contact , Date) VALUE ("'+req.body.rname+'" ,"'+req.body.remail+'" , "'+req.body.rcontact+'" , "'+req.body.rage+'" )' , (err , result) =>{
+    if(err) throw err ;
+    console.log('Succesful')
+    res.redirect("/search");
+  });
+});
+app.post('/search' , (req , res)=>{
+  sql.query('SELECT * FROM employee WHERE '+req.body.column+' = "'+req.body.element+'" ' , (err , result)=>{
+    const obj = JSON.stringify(result);
+    res.render('show', {result});
+    console.log(result);
+    res.end();
+  });
+});
+app.listen(port);
---
+++ public/js/next.js
@@ -0,0 +1,12 @@
+function next() {
+  document.getElementById('hide').style.display = 'block' ;
+  document.getElementById('step').style.display = 'none';
+  document.getElementById('none').style.display = 'block';
+  document.getElementById('block').style.display = 'none';
+}
+function pre() {
+  document.getElementById('hide').style.display = 'none' ;
+  document.getElementById('step').style.display = 'block';
+  document.getElementById('none').style.display = 'none';
+  document.getElementById('block').style.display = 'block';
+}
